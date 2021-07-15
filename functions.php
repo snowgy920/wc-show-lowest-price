ae9dc3ebade09376a018dd385aab7042f0ab5883
Nice Worker <48841351+snowgy920@users.noreply.github.com>
2021-07-15 18:48:12 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,34 +16,6 @@ function porto_child_css() {
 	}
 }
 
-add_action( 'wp_enqueue_scripts', 'porto_child_js', 1002 );
-function porto_child_js() {
-	wp_register_script( 'porto-child', esc_url( get_stylesheet_directory_uri() ) . '/custom.js', array('jquery', 'porto-theme-async'), '', true);
-	wp_enqueue_script( 'porto-child' );
-}
-
-add_action( 'init', 'porto_child_init', 20 );
-function porto_child_init() {
-	add_filter( 'script_loader_tag', 'porto_child_script_add_async_attribute', 10, 2 );
-}
-function porto_child_script_add_async_attribute( $tag, $handle ) {
-	// add script handles to the array below
-	$scripts_to_async = array( 'porto-child' );
-	if ( in_array( $handle, $scripts_to_async ) ) {
-		return str_replace( ' src', ' async="async" src', $tag );
-	}
-	return $tag;
-}
-
-add_action('wp_footer', 'porto_child_render_register_form');
-function porto_child_render_register_form() {
-	if ( ! is_checkout() && ! is_user_logged_in() && ( ! isset( $porto_settings['woo-account-login-style'] ) || ! $porto_settings['woo-account-login-style'] ) ) {
-		echo '<div id="login-form-popup" class="lightbox-content mfp-hide">';
-		echo wc_get_template_part( 'myaccount/form-register-popup' );
-		echo '</div>';
-	}
-}
-
 add_filter( 'woocommerce_variable_sale_price_html', 'wpglorify_variation_price_format', 10, 2 );
 add_filter( 'woocommerce_variable_price_html', 'wpglorify_variation_price_format', 10, 2 );
 function wpglorify_variation_price_format( $price, $product ) {
